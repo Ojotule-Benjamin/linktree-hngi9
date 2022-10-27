dd9a4e7a030d7bf208b7fb42d4edca0a61b1eef4
Ojotule Benjamin Ajodo <benjamin.ojotule.ajodo@gmail.com>
2022-10-27 17:41:38 +0100

    final correction

--- src/components/SocialSection.jsx
+++ src/components/SocialSection.jsx
@@ -6,8 +6,23 @@ export const SocialSection = () => {
   return (
     <div className="socialContainer">
       <div className="socialItems">
-        <SlackIconSvg />
-        <GithubIconSvg />
+        <a
+          id="slack"
+          href="https://app.slack.com/client/T042F7V19Q8/C041JU70S5U/rimeto_profile/U048BCB1N6Q"
+          target={"_blank"}
+          rel="noreferrer"
+        >
+          <SlackIconSvg />
+        </a>
+
+        <a
+          id="slack"
+          href="https://github.com/Ojotule-Benjamin"
+          target={"_blank"}
+          rel="noreferrer"
+        >
+          <GithubIconSvg />
+        </a>
       </div>
     </div>
   );
